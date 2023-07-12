d6ac2518ccca34bcd60a07a2c73809d8500cea9a
MYuriy1970 <109099590+MYuriy1970@users.noreply.github.com>
2023-07-12 22:01:01 +0400

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -5,13 +5,13 @@ import "./style.css";
 
 const Resume = () => {
   return (
-    <div class="resume-wrapper">
-      <section class="profile section-padding">
-        <div class="container">
-          <div class="picture-resume-wrapper">
-            <div class="picture-resume">
+    <div className="resume-wrapper">
+      <section className="profile section-padding">
+        <div className="container">
+          <div className="picture-resume-wrapper">
+            <div className="picture-resume">
               <span>
-                <img src={image} alt="myfoto" className="myfoto" />
+                <img src={image} alt="myfoto" classNameName="myfoto" />
               </span>
               <svg version="1.1" viewBox="0 0 350 350">
                 <defs>
@@ -22,39 +22,39 @@ const Resume = () => {
                 </defs>
 
                 <g filter="url(#goo)">
-                  <circle id="main_circle" class="st0" cx="171.5" cy="175.6" r="130" />
+                  <circle id="main_circle" className="st0" cx="171.5" cy="175.6" r="130" />
 
-                  <circle id="circle" class="bubble0 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble1 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble2 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble3 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble4 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble5 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble6 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble7 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble8 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble9 st1" cx="171.5" cy="175.6" r="122.7" />
-                  <circle id="circle" class="bubble10 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble0 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble1 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble2 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble3 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble4 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble5 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble6 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble7 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble8 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble9 st1" cx="171.5" cy="175.6" r="122.7" />
+                  <circle id="circle" className="bubble10 st1" cx="171.5" cy="175.6" r="122.7" />
                 </g>
               </svg>
             </div>
-            <div class="clearfix"></div>
+            <div className="clearfix"></div>
           </div>
-          <div class="name-wrapper">
+          <div className="name-wrapper">
             <h1>
               Межеедов <br />
               Юрий
             </h1>
           </div>
-          <div class="clearfix"></div>
-          <div class="contact-info clearfix">
-            <ul class="list-titles">
+          <div className="clearfix"></div>
+          <div className="contact-info clearfix">
+            <ul className="list-titles">
               <li>Телефон</li>
               <li>Почта</li>
               <li>Web</li>
               <li>Адрес</li>
             </ul>
-            <ul class="list-content ">
+            <ul className="list-content ">
               <li>
                 <a href="tel:+79991716526">+79991716526</a>
               </li>
@@ -65,7 +65,7 @@ const Resume = () => {
               <li>Россия, Тольятти</li>
             </ul>
           </div>
-          <div class="contact-presentation">
+          <div className="contact-presentation">
             <p>
               Мужчина, 52 года, год рождения- 1970, проживаю в г.Тольятти,
               <br />
@@ -74,12 +74,12 @@ const Resume = () => {
               <strong>График работы:</strong> полный день, удаленная работа
             </p>
           </div>
-          <div class="contact-social clearfix">
-            <ul class="list-titles">
+          <div className="contact-social clearfix">
+            <ul className="list-titles">
               <li>VK</li>
               <li>github</li>
             </ul>
-            <ul class="list-content">
+            <ul className="list-content">
               <li>
                 <a href="https://vk.com/feed">Вконтакте</a>
               </li>
@@ -91,62 +91,62 @@ const Resume = () => {
         </div>
       </section>
 
-      <section class="experience section-padding">
-        <div class="container">
-          <h3 class="experience-title">Experience</h3>
+      <section className="experience section-padding">
+        <div className="container">
+          <h3 className="experience-title">Experience</h3>
 
-          <div class="experience-wrapper">
-            <div class="company-wrapper clearfix">
-              <div class="experience-title">Company name</div>
-              <div class="time">Nov 2012 - Present</div>
+          <div className="experience-wrapper">
+            <div className="company-wrapper clearfix">
+              <div className="experience-title">Company name</div>
+              <div className="time">Nov 2012 - Present</div>
             </div>
 
-            <div class="job-wrapper clearfix">
-              <div class="experience-title">Front End Developer </div>
-              <div class="company-description">
+            <div className="job-wrapper clearfix">
+              <div className="experience-title">Front End Developer </div>
+              <div className="company-description">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a elit facilisis, adipiscing leo in, dignissim magna.</p>
               </div>
             </div>
 
-            <div class="company-wrapper clearfix">
-              <div class="experience-title">Company name</div>
-              <div class="time">Nov 2010 - Present</div>
+            <div className="company-wrapper clearfix">
+              <div className="experience-title">Company name</div>
+              <div className="time">Nov 2010 - Present</div>
             </div>
 
-            <div class="job-wrapper clearfix">
-              <div class="experience-title">Freelance, Web Designer / Web Developer</div>
-              <div class="company-description">
+            <div className="job-wrapper clearfix">
+              <div className="experience-title">Freelance, Web Designer / Web Developer</div>
+              <div className="company-description">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a elit facilisis, adipiscing leo in, dignissim magna.</p>
               </div>
             </div>
 
-            <div class="company-wrapper clearfix">
-              <div class="experience-title">Company name</div>
-              <div class="time">Nov 2009 - Nov 2010</div>
+            <div className="company-wrapper clearfix">
+              <div className="experience-title">Company name</div>
+              <div className="time">Nov 2009 - Nov 2010</div>
             </div>
 
-            <div class="job-wrapper clearfix">
-              <div class="experience-title">Web Designer </div>
-              <div class="company-description">
+            <div className="job-wrapper clearfix">
+              <div className="experience-title">Web Designer </div>
+              <div className="company-description">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a elit facilisis, adipiscing leo in, dignissim magna.</p>
               </div>
             </div>
           </div>
 
-          <div class="section-wrapper clearfix">
-            <h3 class="section-title">Skills</h3>
+          <div className="section-wrapper clearfix">
+            <h3 className="section-title">Skills</h3>
             <ul>
-              <li class="skill-percentage">HTML / HTML5</li>
-              <li class="skill-percentage">CSS / CSS3 / SASS / LESS</li>
-              <li class="skill-percentage">Javascript</li>
-              <li class="skill-percentage">Jquery</li>
-              <li class="skill-percentage">Wordpress</li>
-              <li class="skill-percentage">PHP</li>
+              <li className="skill-percentage">HTML / HTML5</li>
+              <li className="skill-percentage">CSS / CSS3 / SASS / LESS</li>
+              <li className="skill-percentage">Javascript</li>
+              <li className="skill-percentage">Jquery</li>
+              <li className="skill-percentage">Wordpress</li>
+              <li className="skill-percentage">PHP</li>
             </ul>
           </div>
 
-          <div class="section-wrapper clearfix">
-            <h3 class="section-title">Hobbies</h3>
+          <div className="section-wrapper clearfix">
+            <h3 className="section-title">Hobbies</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a elit facilisis, adipiscing leo in, dignissim magna.</p>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a elit facilisis, adipiscing leo in, dignissim magna.</p>
@@ -154,7 +154,7 @@ const Resume = () => {
         </div>
       </section>
 
-      <div class="clearfix"></div>
+      <div className="clearfix"></div>
     </div>
   );
 };
